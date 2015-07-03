9797dc66d45c147522854f6e0bf38b8d89338aa1
Lalit Maganti <lalitmaganti@gmail.com>
2015-07-03 01:22:26 +0100

    assert: fix assertions

--- relay/src/test/kotlin/co/fusionx/relay/test/assertions.kt
+++ relay/src/test/kotlin/co/fusionx/relay/test/assertions.kt
@@ -3,7 +3,8 @@ package co.fusionx.relay.test
 import org.assertj.core.api.AbstractAssert
 import org.assertj.core.api.AbstractIterableAssert
 
-public fun assertThat<T>(iterable: T): AbstractAssert<*, T> =
-    org.assertj.core.api.Assertions.assertThat(iterable)
+public class ObjectAssert<T>(actual: T) : org.assertj.core.api.ObjectAssert<T>(actual)
+
+public fun assertThat<T>(obj: T): AbstractAssert<*, T> = ObjectAssert<T>(obj)
 public fun assertThat<T>(iterable: Iterable<T>): AbstractIterableAssert<*, out Iterable<T>, T> =
     org.assertj.core.api.Assertions.assertThat(iterable)
