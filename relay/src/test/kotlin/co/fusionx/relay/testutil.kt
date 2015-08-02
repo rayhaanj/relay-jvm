7e7646479fa118fba4676ae0ac9acd43f02d20fc
Lalit Maganti <lalitmaganti@gmail.com>
2015-08-02 01:38:28 +0100

    test: fix build

--- relay/src/test/kotlin/co/fusionx/relay/testutil.kt
+++ relay/src/test/kotlin/co/fusionx/relay/testutil.kt
@@ -7,7 +7,7 @@ import rx.observers.TestSubscriber
 public fun uninitialized<T>(): T = null as T
 
 public inline fun mock<reified T>(): T = Mockito.mock(javaClass<T>())
-public inline fun on<reified T>(obj: T): OngoingStubbing<T> = Mockito.on(obj)
+public inline fun on<reified T>(obj: T): OngoingStubbing<T> = Mockito.`when`(obj)
 
 public fun <T> TestSubscriber<T>.assertValuesCompletedNoErrors(vararg values: T) {
     assertValues(*values)
